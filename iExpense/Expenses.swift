95a9f4781ef29cc217630026e4a19ca33f22df8c
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2022-06-21 23:38:22 +0800

    Uploaded files

---
+++ iExpense/Expenses.swift
@@ -0,0 +1,28 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Adam Chen JingFan on 2022/6/10.
+//
+
+import Foundation
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Adam Chen JingFan on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
